0f7becf4eada01513efe4a6c814bad3df3db83fc
Maximiliano <maxirpc2607@gmail.com>
2021-11-01 17:31:40 -0300

    update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,12 @@
 import GetHeroes from "../../services/GetHeroes";
 
 export default function Home() {
-	const heroes = GetHeroes();
+  const heroes = GetHeroes();
+  console.log(heroes);
 
-	return (
-		<>
-			<h1>Home</h1>
-		</>
-	);
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
 }
--- src/services/GetHeroes.js
+++ src/services/GetHeroes.js
@@ -2,17 +2,17 @@ import axios from "axios";
 import { API } from "./Api";
 
 export default function GetHeroes() {
-	const heroe = {
-		id: "",
-		name: "",
-		image: "",
-	};
-	axios.get(API + 1).then((res) => {
-		let hero = res.data;
-		heroe.id = hero.id;
-		heroe.name = hero.name;
-		heroe.image = hero.image.url;
-	});
+  let heroe = {
+    id: "",
+    name: "",
+    image: "",
+  };
+  axios.get(API + 1).then((res) => {
+    let hero = res.data;
+    heroe.id = hero.id;
+    heroe.name = hero.name;
+    heroe.image = hero.image.url;
+  });
 
-	return heroe;
+  return heroe;
 }
