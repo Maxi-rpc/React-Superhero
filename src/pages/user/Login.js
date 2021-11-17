5a47b262af59b5f8ca4a24282e5a1d79df16f372
Maximiliano <maxirpc2607@gmail.com>
2021-11-16 23:53:53 -0300

    Update Login.js

--- src/pages/user/Login.js
+++ src/pages/user/Login.js
@@ -1,21 +1,13 @@
-import Banner from "../../components/banner/Banner.js";
 import FormLogin from "../../components/form/Login.js";
-import { GetHeroDefault, GetHero } from "../../services/GetHero.js";
 
 export default function Login() {
-  const heroe = GetHeroDefault();
-  const her = GetHero();
-  console.log(her);
-  return (
-    <>
-      <div className="row py-5 justify-content-md-center align-items-center">
-        <div className="col-md-4">
-          <FormLogin></FormLogin>
-        </div>
-        <div className="col-md-4">
-          <Banner image={heroe.image.url} width="50%" height=""></Banner>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="row my-5 py-5 justify-content-md-center align-items-center">
+				<div className="col-md-4">
+					<FormLogin></FormLogin>
+				</div>
+			</div>
+		</>
+	);
 }
