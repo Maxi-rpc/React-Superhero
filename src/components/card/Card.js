2ce973a519d6a4dfcc1c853b72362131d50ee9d1
Maximiliano <maxirpc2607@gmail.com>
2021-11-20 18:44:07 -0300

    Home falta test

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,19 +1,36 @@
 export const Card = ({ nombre, imagen, powerStats }) => {
-  return (
-    <>
-      <div className="card">
-        <div className="card-header">{nombre}</div>
-        <img src={imagen} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <p className="card-text">powerStats</p>
-          <a href="#" className="btn btn-sm btn-info">
-            Detalle
-          </a>
-          <a href="#" className="btn btn-sm btn-danger">
-            Eliminar
-          </a>
-        </div>
-      </div>
-    </>
-  );
+	let powers = {
+		combat: powerStats.combat,
+		durability: powerStats.durability,
+		intelligence: powerStats.intelligence,
+		power: powerStats.power,
+		speed: powerStats.speed,
+		strength: powerStats.strength,
+	};
+
+	let listPowers = Object.keys(powers).map((keyname, index) => (
+		<li class="list-group-item text-uppercase">
+			{keyname} <span class="badge bg-primary">{powers[keyname]}</span>
+		</li>
+	));
+
+	return (
+		<>
+			<div className="card mb-5">
+				<h5 className="card-header">{nombre}</h5>
+				<img src={imagen} className="card-img-top" alt={nombre} />
+				<div className="card-body">
+					<ul class="list-group list-group-flush">{listPowers}</ul>
+				</div>
+				<div class="card-footer text-center">
+					<button className="btn btn-sm btn-info">
+						<i class="fas fa-search"></i>
+					</button>
+					<button className="btn btn-sm btn-danger">
+						<i class="fas fa-trash-alt"></i>
+					</button>
+				</div>
+			</div>
+		</>
+	);
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,22 @@
 import { Title } from "../../components/title/Title";
 import { Card } from "../../components/card/Card";
+import { GetHeroDefault } from "../../services/GetHero";
 
 export default function Home() {
-  return (
-    <>
-      <Title titulo="Home"></Title>
-      <div className="row">
-        <div className="col-3">
-          <Card nombre="batman" imagen=""></Card>
-        </div>
-      </div>
-    </>
-  );
+	const heroe = GetHeroDefault();
+
+	return (
+		<>
+			<Title titulo="Home"></Title>
+			<div className="row">
+				<div className="col-2">
+					<Card
+						nombre={heroe.name}
+						imagen={heroe.image.url}
+						powerStats={heroe.powerstats}
+					></Card>
+				</div>
+			</div>
+		</>
+	);
 }
