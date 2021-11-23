4048181c5561a1871dc84c247f0d76f33611dd14
Maximiliano <maxirpc2607@gmail.com>
2021-11-23 17:02:40 -0300

    Update SearchBar.js

--- src/components/form/SearchBar.js
+++ src/components/form/SearchBar.js
@@ -2,44 +2,45 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import { GetHero } from "../../services/GetHero";
 
 export const SearchBar = () => {
-	let resultado = {};
+  let resultado = {};
 
-	return (
-		<>
-			<Formik
-				// valor del input
-				initialValues={{
-					search: "",
-				}}
-				validate={(valores) => {
-					let errores = {};
+  return (
+    <>
+      <Formik
+        // valor del input
+        initialValues={{
+          search: "",
+        }}
+        validate={(valores) => {
+          let errores = {};
 
-					return errores;
-				}}
-				onSubmit={(valores, { resetForm }) => {
-					resetForm();
-					resultado = GetHero(valores.search);
-					console.log(valores);
-					console.log("form submit");
-				}}
-			>
-				{({ errors }) => (
-					<Form>
-						<div className="d-flex">
-							<Field
-								className="form-control me-2"
-								type="search"
-								placeholder="Buscar"
-								aria-label="Search"
-								id="search"
-								name="search"
-							/>
-							<button className="btn btn-outline-info">Buscar</button>
-						</div>
-					</Form>
-				)}
-			</Formik>
-			<div className="alert">{console.log(resultado)}</div>
-		</>
-	);
+          return errores;
+        }}
+        onSubmit={(valores, { resetForm }) => {
+          resetForm();
+          resultado = GetHero(valores.search);
+          console.log(valores);
+          console.log(resultado);
+          console.log("form submit");
+        }}
+      >
+        {({ errors }) => (
+          <Form>
+            <div className="d-flex">
+              <Field
+                className="form-control me-2"
+                type="search"
+                placeholder="Buscar"
+                aria-label="Search"
+                id="search"
+                name="search"
+              />
+              <button className="btn btn-outline-info">Buscar</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      <div className="alert">{console.log(resultado)}</div>
+    </>
+  );
 };
